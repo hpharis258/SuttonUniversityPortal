ea46710a0e8ae30c709d4708724e84613b3fe2dd
Haroldas <hpharis258@gmail.com>
2023-03-26 19:16:26 +0100

    first commit

---
+++ suttonportal/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "suttonportaldc50d665": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ suttonportal/cypress/e2e/spec.cy.js
@@ -0,0 +1,28 @@
+describe('Authenticator:', function() {
+  // Step 1: setup the application state
+  beforeEach(function() {
+    cy.visit('http://localhost:3000/');
+  });
+  
+  describe('Sign In:', () => {
+    it('allows a user to signin', () => {
+      // Step 2: Take an action (Sign in)
+      cy.get('input:first').type("hpharis258@gmail.com");
+      cy.get('input:last').type("wWzxdzJLn5S4F@j");
+      cy.get(selectors.signInButton).contains('Sign in').click();
+
+      cy.wait(3000);
+
+      // Step 3: Make an assertion (Check for sign-out text)
+        cy.get(selectors.signOutButton).contains('Sign out');
+    });
+  });
+
+});
+export const selectors = {
+  // Auth component classes
+  usernameInput: '[data-amplify-usernamealias="true"]',
+  signInPasswordInput: '[data-amplify-password="true"]',
+  signInButton: '[data-variation="primary"]',
+  signOutButton: 'button'
+}
