68d1249e7768bce7d16c760b96ca96f4103bde8d
Haroldas <hpharis258@gmail.com>
2023-03-27 20:17:25 +0100

    things happen

--- suttonportal/cypress/e2e/spec.cy.js
+++ suttonportal/cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 describe('Authenticator:', function() {
   // Step 1: setup the application state
   beforeEach(function() {
-    cy.visit('https://github.com/hpharis258/SuttonUniversityPortal/tree/main');
+    cy.visit('http://localhost:3000');
   });
   
   describe('Sign In:', () => {
